fe65da12ecff7bd7880330c19c191e8e45b963e3
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2025-05-16 22:08:05 +0300

    Create __init__.py

---
+++ interface/mcp_server/plugins/devops/__init__.py
@@ -0,0 +1,9 @@
+from .deployment_automation import DeploymentAutomationPlugin
+from .infrastructure_as_code import InfrastructureAsCodePlugin
+from .monitoring_integration import MonitoringIntegrationPlugin
+
+__all__ = [
+    'DeploymentAutomationPlugin',
+    'InfrastructureAsCodePlugin',
+    'MonitoringIntegrationPlugin'
+]
