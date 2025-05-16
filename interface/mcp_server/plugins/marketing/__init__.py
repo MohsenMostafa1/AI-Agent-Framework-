1d2160ab59b9bf59640b8dd86c68958ce14074c7
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2025-05-16 22:14:04 +0300

    Create __init__.py

---
+++ interface/mcp_server/plugins/marketing/__init__.py
@@ -0,0 +1,9 @@
+from .campaign_analysis import CampaignAnalysisPlugin
+from .audience_targeting import AudienceTargetingPlugin
+from .seo_optimization import SEOOptimizationPlugin
+
+__all__ = [
+    'CampaignAnalysisPlugin',
+    'AudienceTargetingPlugin',
+    'SEOOptimizationPlugin'
+]
