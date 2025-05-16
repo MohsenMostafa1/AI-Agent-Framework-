c1b32f39c50649aeba15d9d63c8a8200f06e316d
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2025-05-16 22:12:10 +0300

    Create __init__.py

---
+++ interface/mcp_server/plugins/coder/__init__.py
@@ -0,0 +1,9 @@
+from .code_generation import CodeGenerationPlugin
+from .code_review import CodeReviewPlugin
+from .documentation_tools import DocumentationPlugin
+
+__all__ = [
+    'CodeGenerationPlugin',
+    'CodeReviewPlugin',
+    'DocumentationPlugin'
+]
