35e448c2fcce49b3ed03cf5677889179360e6b8e
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2025-05-16 22:10:10 +0300

    Create __init__.py

---
+++ interface/mcp_server/plugins/ecommerce/__init__.py
@@ -0,0 +1,9 @@
+from .product_recommendations import ProductRecommendationsPlugin
+from .cart_optimization import CartOptimizationPlugin
+from .customer_segmentation import CustomerSegmentationPlugin
+
+__all__ = [
+    'ProductRecommendationsPlugin',
+    'CartOptimizationPlugin',
+    'CustomerSegmentationPlugin'
+]
